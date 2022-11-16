c4e261362de41a6cd200b667e942f5154e6e548e
MagnusKristian <92784711+MagnusKristian@users.noreply.github.com>
2022-11-16 10:05:24 +0100

    menustyle

--- Login.cs
+++ Login.cs
@@ -14,27 +14,56 @@ public class Login
         while (validChoice == false)
         {
             Console.Clear();
-            Console.WriteLine("Welcome to FriendFace!");
-            Console.WriteLine("Type '1' to sign in:");
-            Console.WriteLine("Type '2' to sign up:");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("****************************************");
+            Console.WriteLine("*                                      *");
+            Console.WriteLine("*                                      *");
+            Console.WriteLine("*                                      *");
+            Console.WriteLine("*        Welcome to FriendFace!        *");
+            Console.WriteLine("*        Type '1' to sign in:          *");
+            Console.WriteLine("*        Type '2' to sign up:          *");
+            Console.WriteLine("*                                      *");
+            Console.WriteLine("*                                      *");
+            Console.WriteLine("*                                      *");
+            Console.WriteLine("****************************************");
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.Cyan;
+
             logInChoice = Console.ReadLine();
-            if(logInChoice == "1" || logInChoice == "2")
+            Console.ResetColor();
+
+            if (logInChoice == "1" || logInChoice == "2")
             {
                 validChoice = true;
+                Console.Clear();
                 break;
             }
         }
+        Console.ResetColor();
         
         bool userFound= false;
         bool logInOk = false;
         if (logInChoice == "1")
         {
-            Console.WriteLine("What is your name?(username)");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("****************************************");
+            Console.WriteLine("*                                      *");
+            Console.WriteLine("*   What is your name?(Not username)   *");
+            Console.WriteLine("*                                      *");
+            Console.WriteLine("****************************************");
+            Console.WriteLine();
+            Console.ResetColor();
+
             string username = "";
             Profile chosenUser = null;
             while (userFound == false)
             {
                 username = Console.ReadLine();
+                Console.Clear();
                 for (int i = 0; i < friendFace.ListOfAllUsers.Count; i++)
                 {
                     if (username.ToLower() == friendFace.ListOfAllUsers[i].Name.ToLower())
@@ -59,14 +88,27 @@ public class Login
                 }
                 if (userFound == false)
                 {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
                     Console.WriteLine("User not found. Try again.");
-                    Console.WriteLine("What is your name?(username)");
+                    Console.ResetColor();
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.WriteLine();
+                    Console.WriteLine("****************************************");
+                    Console.WriteLine("*                                      *");
+                    Console.WriteLine("*   What is your name?(Not username)   *");
+                    Console.WriteLine("*                                      *");
+                    Console.WriteLine("****************************************");
+                    Console.WriteLine();
+                    Console.ResetColor();
                 }
             }
             if (chosenUser != null && userFound == true)
             {
-                Console.WriteLine("---------------");
-                Console.WriteLine($"Found user '{chosenUser.Name}', Logging in...");
+                //Console.WriteLine("---------------");
+                //Console.WriteLine($"Found user '{chosenUser.Name}', Logging in...");
                 friendFace.SetCurrentUser(chosenUser);
             }
 
--- Menu.cs
+++ Menu.cs
@@ -78,6 +78,8 @@ public class Menu
                 default:
                     break;
             }
+            //Console.Clear();
+
         }
     }
 
