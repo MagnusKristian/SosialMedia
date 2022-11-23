2bfaa188e5d00f63017e9817c6459af428ebeaef
MagnusKristian <92784711+MagnusKristian@users.noreply.github.com>
2022-11-23 15:57:01 +0100

    close but no cigar

--- PostHandler.cs
+++ PostHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace SosialMedia;
+﻿using System.Threading.Channels;
+
+namespace SosialMedia;
 
 public class PostHandler
 {
@@ -39,72 +41,73 @@ public class PostHandler
         string space = " ";
         string frameTopBottom = star.PadRight(40,'*');
         string spaceFrame = star.PadRight(39, ' ') + star;
-        string textLine = (star + space + postText).PadRight(39, ' ') + star;
-        //string textLine;
-        //bool twoLines = false;
-        //string lineOne = "";
-        //string lineTwo = "";
-        //string textToPrint = "";
+        string shortPostText = "";
+        //string shortTextLine = (star + space + shortPostText).PadRight(39, ' ') + star;
         string nameLine = (star + space +"Name: "+ name).PadRight(39, ' ') + star;
         string postTimeLine = (star + space + "Time posted: " + postTime).PadRight(39, ' ') + star;
-        if (postText.Length > 36)
-        {
-            int numOfLinesNeeded = 0;
-            string[] lineStrings;
-            for (var i=1;i<10;i++)
-            {
-                if (postText.Length / i > 36)
-                {
-                    numOfLinesNeeded ++;
-                }
+        
 
-            }
-            lineStrings = new string[numOfLinesNeeded];
-            int lineStart = 0;
-            int lineLength = 36;
-            for (int i = 0; i < numOfLinesNeeded; i++)
-            {
-                lineStrings[i] = postText.Substring(lineStart, lineLength);
-                lineStart += 36;
-            }
-
-            foreach (var line in lineStrings)
-            {
-                Console.WriteLine($"{(star + space + line).PadRight(39, ' ') + star}");
-            }
-
-            //TODO: hvis det er verdt å bruke tid på etter du er ferdig med det andre:
-            //kan lage noe med modulo for å ta inn og splitte opp på flere linjer uavhengig av antall chars osv. 
-            //eller sette maks antall chars på posts.
-            
-            //lineOne = (star+space+postText.Substring(0, postText.Length / 2)).PadRight(39, ' ') + star;
-            //lineTwo = (star+space+postText.Substring(postText.Length / 2)).PadRight(39, ' ') + star;
-            //twoLines = true;
-            //Console.WriteLine("text too long, fix it....");
-        }
-        else
-        {
-            textLine = (star + space + postText).PadRight(39, ' ') + star;
-            Console.WriteLine();
-        }
+        
         Console.WriteLine($"{frameTopBottom}");//star
         Console.WriteLine($"{spaceFrame}");//space
         Console.WriteLine($"{nameLine}");//name
         Console.WriteLine($"{spaceFrame}");//space
-        //if (twoLines)
-        //{
-        //    Console.WriteLine($"{lineOne}"); //text
-        //    Console.WriteLine($"{lineTwo}"); //text
-        //}
-        //else
-        //{
-        //    Console.WriteLine($"{textLine}");//text
-        //}
+        LongOrShortPostText(postText);
         Console.WriteLine($"{spaceFrame}");//space
         Console.WriteLine($"{spaceFrame}");//space
         Console.WriteLine($"{spaceFrame}");//space
         Console.WriteLine($"{postTimeLine}");//post time
         Console.WriteLine($"{spaceFrame}");//space
         Console.WriteLine($"{frameTopBottom}");//star
+    }
+
+    private static void LongOrShortPostText(string postText)
+    {
+        //Console.WriteLine("LongOrShortPostText KJØRTE");
+        string star = "*";
+        string space = " ";
+        //string shortPostText = "xxxx";
+        string shortTextLine = (star + space + postText).PadRight(39, ' ') + star;
+        if (postText.Length <= 36)
+        {
+            //shortPostText = postText;
+            Console.WriteLine($"{shortTextLine}");
+            //Console.WriteLine("NEIIIIIII DEN ER IKKE LENGER ENN 36");
+        }
+        if (postText.Length > 36)
+        {
+            //Console.WriteLine("JAAAA DEN ER LENGER ENN 36");
+            int numOfLinesNeeded = 1;
+            string[] lineStrings;
+            for (var i = 1; i < 10; i++)
+            {
+                if (postText.Length / i > 36)
+                {
+                    numOfLinesNeeded++;
+                }
+            }
+            lineStrings = new string[numOfLinesNeeded];
+            int lineStart = 0;
+            int lineLength = 35;
+            for (int i = 1; i < numOfLinesNeeded; i++)
+            {
+                
+                if (numOfLinesNeeded > i+1)
+                {
+                    lineStrings[i] = postText.Substring(lineStart);
+                    lineStart += 36;
+                }
+
+                if (i + 1 < numOfLinesNeeded)
+                {
+                    lineStrings[i] = postText.Substring(lineStart, lineLength);
+                }
+                
+            }
+            foreach (var line in lineStrings)
+            {
+                Console.WriteLine($"{(star + space + line).PadRight(39, ' ') + star}");
+            }
+        }
     }
 }
